5b17d656a2142482fe96f31d84f6bee0134bc4bd
Raditya Saskara <raditya.saskara@gdn-commerce.com>
2021-06-11 21:32:22 +0700

    adding cookie-session

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,42 +1,56 @@
-import express from "express";
-import "express-async-errors";
-import { json } from "body-parser";
-import mongoose from "mongoose";
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
-import { currentUserRouter } from "./routes/current-user";
-import { signInRouter } from "./routes/signin";
-import { signUpRouter } from "./routes/signup";
-import { signOutRouter } from "./routes/signout";
-import { errorHandler } from "./routes/middlewares/error-handler";
-import { RouteError } from "./errors/route-error";
+import { currentUserRouter } from './routes/current-user';
+import { signInRouter } from './routes/signin';
+import { signUpRouter } from './routes/signup';
+import { signOutRouter } from './routes/signout';
+import { errorHandler } from './routes/middlewares/error-handler';
+import { RouteError } from './errors/route-error';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
 
-app.all("*", async (req, res) => {
+app.all('*', async (req, res) => {
   throw new RouteError();
 });
 
 app.use(errorHandler);
 
 const init = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT must be defined');
+  }
+
   try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
     });
-    console.log("Connected");
+    console.log('Connected');
   } catch (error) {
     console.error(error);
   }
 
-  app.listen(3000, () => console.log("---AUTH---,Listening on port 3000"));
+  app.listen(3000, () => console.log('---AUTH---,Listening on port 3000'));
 };
 
 init();
+
+// kubectl create secret generic jwt-secret --from-literal=JWT_KEY=radrad
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,23 +1,25 @@
-import express, { Request, Response } from "express";
-import { User } from "../models/user";
-import { body, validationResult } from "express-validator";
-import { RequestValidationError } from "../errors/request-validation-error";
-import { BadRequestError } from "../errors/bad-request-errors";
+import express, { Request, Response } from 'express';
+import { User } from '../models/user';
+import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { RequestValidationError } from '../errors/request-validation-error';
+import { BadRequestError } from '../errors/bad-request-errors';
 
 const router = express.Router();
 
 router.post(
-  "/api/users/signup",
+  '/api/users/signup',
   [
-    body("email").isEmail().withMessage("Email must be valid"),
-    body("password")
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
       .trim()
       .isLength({ min: 4, max: 20 })
-      .withMessage("Password must be between 4 and 20 characters"),
-    body("username")
+      .withMessage('Password must be between 4 and 20 characters'),
+    body('username')
       .trim()
       .isLength({ min: 4, max: 20 })
-      .withMessage("Username must be between 4 and 20 characters"),
+      .withMessage('Username must be between 4 and 20 characters'),
   ],
   async (req: Request, res: Response) => {
     const err = validationResult(req);
@@ -30,16 +32,30 @@ router.post(
 
     let existingUser = await User.findOne({ email });
     if (existingUser) {
-      throw new BadRequestError("Email already used");
+      throw new BadRequestError('Email already used');
     } else {
       existingUser = await User.findOne({ username });
       if (existingUser) {
-        throw new BadRequestError("Username already used");
+        throw new BadRequestError('Username already used');
       }
     }
 
     const user = User.build({ username, email, password });
     await user.save();
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        username: user.username,
+      },
+      process.env.JWT_KEY!
+    );
+
+    // Store on session
+    req.session = {
+      jwt: userJwt,
+    };
 
     res.status(201).send(user);
   }
