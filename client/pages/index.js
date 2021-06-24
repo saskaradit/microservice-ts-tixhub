c25e772339898f03f37c2e453964d6fef431952f
saskaradit <saskaradit@gmail.com>
2021-06-24 21:50:29 +0700

    fixing ingress

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,29 @@
-const landing = () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+
+  axios.get('/api/users/currentuser').catch((err) => {
+    console.log(err.message);
+  });
   return <h1>Landing Page</h1>;
 };
 
-export default landing;
+LandingPage.getInitialProps = async () => {
+  if (typeof window === 'undefined') {
+    // we are on the server
+    // request made to http://ingress-nginx.ingress-nginx.svc.cluster.local
+  } else {
+    // we are on the browser
+    // request only to the context path
+  }
+  const response = await axios.get(
+    'http://ingress-nginx.ingress-nginx.svc.cluster.local/api/users/currentuser'
+  );
+
+  console.log('Im on the landing page');
+
+  return response.data;
+};
+
+export default LandingPage;
