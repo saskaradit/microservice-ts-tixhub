323a73cf6944894026fc0d1e3c6121cfc6c13a3b
saskaradit <saskaradit@gmail.com>
2021-06-25 20:51:59 +0700

    cookies with server side

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,28 +2,28 @@ import axios from 'axios';
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
-
-  axios.get('/api/users/currentuser').catch((err) => {
-    console.log(err.message);
-  });
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async ({ req }) => {
   if (typeof window === 'undefined') {
     // we are on the server
     // request made to http://ingress-nginx.ingress-nginx.svc.cluster.local
+    const { data } = await axios.get(
+      'http://ingress-nginx.ingress-nginx-controller.svc.cluster.local/api/users/currentuser/api/users/currentuser',
+      {
+        headers: req.headers,
+      }
+    );
+
+    return data;
   } else {
     // we are on the browser
     // request only to the context path
+    const { data } = await axios.get('/api/users/currentuser');
+
+    return data;
   }
-  const response = await axios.get(
-    'http://ingress-nginx.ingress-nginx.svc.cluster.local/api/users/currentuser'
-  );
-
-  console.log('Im on the landing page');
-
-  return response.data;
 };
 
 export default LandingPage;
