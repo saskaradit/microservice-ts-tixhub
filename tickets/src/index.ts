111de4c7fa1d59fee4be528aac797bcf60a2a37b
saskaradit <saskaradit@gmail.com>
2021-07-07 20:51:19 +0700

    adding tickets service

---
+++ tickets/src/index.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import { app } from './app';
+
+const init = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT must be defined');
+  }
+
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('Connected');
+  } catch (error) {
+    console.error(error);
+  }
+
+  app.listen(3000, () => console.log('---AUTH---,Listening on port 3000'));
+};
+
+init();
+
+// kubectl create secret generic jwt-secret --from-literal=JWT_KEY=radrad
