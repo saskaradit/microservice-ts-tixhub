f996ff95d2d9754d8a0d011e8d1529f353ed9b20
saskaradit <saskaradit@gmail.com>
2021-07-14 20:43:37 +0700

    implanting DDD in nats

---
+++ nats-test/src/events/ticket-created-listener.ts
@@ -0,0 +1,13 @@
+import { Listener } from './listener'
+import { Message } from 'node-nats-streaming'
+
+export class TicketCreatedListener extends Listener {
+  subject = 'ticket:created'
+  queueGroupName = 'payments-service'
+
+  onMessage(data: any, msg: Message) {
+    console.log('Event Data: ', data)
+
+    msg.ack()
+  }
+}
--- nats-test/src/subscriber.ts
+++ nats-test/src/subscriber.ts
@@ -1,5 +1,6 @@
-import nats, { Message } from 'node-nats-streaming'
+import nats from 'node-nats-streaming'
 import { randomBytes } from 'crypto'
+import { TicketCreatedListener } from './events/ticket-created-listener'
 
 const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
   url: 'http://localhost:4222',
@@ -13,24 +14,7 @@ stan.on('connect', () => {
     process.exit()
   })
 
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName('orders-service')
-
-  const subscription = stan.subscribe('ticket:created', 'orders-queue', options)
-
-  subscription.on('message', (msg: Message) => {
-    const data = msg.getData()
-
-    if (typeof data == 'string') {
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`)
-    }
-    console.log('Mesage Received')
-
-    msg.ack()
-  })
+  new TicketCreatedListener(stan).listen()
 })
 
 process.on('SIGINT', () => stan.close())
