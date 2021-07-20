3c5ce4bee26c1815e325040cafcf32e88f01ecd4
saskaradit <saskaradit@gmail.com>
2021-07-20 23:58:38 +0700

    fixing commong modules

--- tickets/src/events/subscribers/order-created-subscriber.ts
+++ tickets/src/events/subscribers/order-created-subscriber.ts
@@ -2,6 +2,7 @@ import { Listener, OrderCreatedEvent, Subjects } from '@rad-sas/common'
 import { Message } from 'node-nats-streaming'
 import { queueGroupName } from './queue-group-name'
 import { Ticket } from '../../models/tickets'
+import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher'
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated
@@ -21,6 +22,14 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
     // save ticket
     await ticket.save()
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      version: ticket.version,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+    })
 
     // ack
     msg.ack()
