a12c097a938128c6cfd9fc7aeed001fa2877ad03
saskaradit <saskaradit@gmail.com>
2021-07-22 22:45:41 +0700

    add expiration delay

--- expiration/src/events/subscribers/order-created-subscriber.ts
+++ expiration/src/events/subscribers/order-created-subscriber.ts
@@ -8,9 +8,15 @@ export class OrderCreatedSubscriber extends Listener<OrderCreatedEvent> {
   queueGroupName = queueGroupName
 
   async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
-    await expirationQueue.add({
-      orderId: data.id,
-    })
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime()
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      {
+        delay,
+      }
+    )
 
     msg.ack()
   }
