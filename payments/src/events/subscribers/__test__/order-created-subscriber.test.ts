06727a2bff4aee204169c548bb74b072339ef1ab
saskaradit <saskaradit@gmail.com>
2021-07-23 11:13:14 +0700

    add test and model

---
+++ payments/src/events/subscribers/__test__/order-created-subscriber.test.ts
@@ -0,0 +1,48 @@
+import { natsWrapper } from '../../../nats-wrapper'
+import mongoose from 'mongoose'
+import { OrderCreatedSubscriber } from '../order-created-subscriber'
+import { OrderCreatedEvent, OrderStatus } from '@rad-sas/common'
+import { Message } from 'node-nats-streaming'
+import { Order } from '../../../models/order'
+
+const setup = async () => {
+  const listener = new OrderCreatedSubscriber(natsWrapper.client)
+
+  const data: OrderCreatedEvent['data'] = {
+    id: mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    expiresAt: '123',
+    userId: 'hehehe',
+    status: OrderStatus.Created,
+    ticket: {
+      id: 'ehehe',
+      price: 200,
+    },
+  }
+
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  }
+
+  return { listener, data, msg }
+}
+
+it('replicates the order info', async () => {
+  const { listener, data, msg } = await setup()
+
+  await listener.onMessage(data, msg)
+
+  const order = await Order.findById(data.id)
+  console.log(order)
+
+  expect(order!.price).toEqual(data.ticket.price)
+})
+
+it('replicates the order info', async () => {
+  const { listener, data, msg } = await setup()
+
+  await listener.onMessage(data, msg)
+
+  expect(msg.ack).toHaveBeenCalled()
+})
--- payments/src/test/setup.ts
+++ payments/src/test/setup.ts
@@ -5,11 +5,7 @@ import request from 'supertest'
 import jwt from 'jsonwebtoken'
 
 declare global {
-  namespace NodeJS {
-    interface Global {
-      signin(): string[]
-    }
-  }
+  var signin: (id?: string) => string[]
 }
 
 jest.mock('../nats-wrapper')
