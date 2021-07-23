10b6ef3857715aa901bd3036a246797984b5ec82
saskaradit <saskaradit@gmail.com>
2021-07-23 10:23:50 +0700

    adding payments service

---
+++ payments/src/app.ts
@@ -0,0 +1,24 @@
+import express from 'express'
+import 'express-async-errors'
+import { json } from 'body-parser'
+import cookieSession from 'cookie-session'
+import { RouteError, errorHandler, currentUser } from '@rad-sas/common'
+
+const app = express()
+app.set('trust proxy', true)
+app.use(json())
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+)
+app.use(currentUser)
+
+app.all('*', async (req, res) => {
+  throw new RouteError()
+})
+
+app.use(errorHandler)
+
+export { app }
