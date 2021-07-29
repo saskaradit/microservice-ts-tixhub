bdfbb7cfdbe00e046ea04847bbdae233f025db0f
saskaradit <saskaradit@gmail.com>
2021-07-29 14:18:20 +0700

    furnishing client touches

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,40 +1,34 @@
 import Link from 'next/link'
 
-const LandingPage = ({ currentUser, tickets }) => {
-  const ticketList = tickets.map((ticket) => {
-    return (
-      <tr key={ticket.id}>
-        <td>{ticket.title}</td>
-        <td>{ticket.price}</td>
-        <td>
-          <Link href='/tickets/[ticketId]' as={`/tickets/${ticket.id}`}>
-            <a>View</a>
-          </Link>
-        </td>
-      </tr>
-    )
-  })
-
+const LandingPage = () => {
   return (
     <div>
-      <h1>Tickets</h1>
-      <table className='table'>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Price</th>
-            <th>Link</th>
-          </tr>
-        </thead>
-        <tbody>{ticketList}</tbody>
-      </table>
+      <div class='jumbotron mb-4'>
+        <img
+          src='https://www.monsterenergy.com/media/uploads_image/2017/05/24/1600/800/6667b5e33ecf0270ac29015d09f0558b.jpg?mod=v1_a8a93ca8509709aefa5c593196cdd1a4'
+          alt=''
+          className='card-img-top mb-3'
+        />
+        <h1 class='display-4'>Welcome to Tixhub</h1>
+        <p class='lead'>
+          Tixhub is a marketplace where you can create tickets and create events
+          anytime you want without any strings attached
+        </p>
+        <hr class='my-4' />
+        <p>Try Tixhub Now</p>
+        <Link href='/tickets'>
+          <a class='btn btn-warning' href='#' role='button'>
+            Buy Tickets
+          </a>
+        </Link>
+      </div>
     </div>
   )
 }
 
-LandingPage.getInitialProps = async (context, client, currentUser) => {
-  const { data } = await client.get('/api/tickets')
-  return { tickets: data }
-}
+// LandingPage.getInitialProps = async (context, client, currentUser) => {
+//   const { data } = await client.get('/api/tickets')
+//   return { tickets: data }
+// }
 
 export default LandingPage
